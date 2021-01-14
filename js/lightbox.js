9b9018c75599144e6ac62d5b7dc7c5cd57d999b0
AlonsoAp <68049005+AlonsoAp@users.noreply.github.com>
2021-01-14 12:41:40 -0500

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,9 @@
+const imagenes = document.querySelectorAll('img-galeria')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', () => {
+        alert("ad")
+    })
+})
